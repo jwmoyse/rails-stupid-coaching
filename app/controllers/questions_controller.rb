3df366673a68a4989ab2082026083c2fc04fbed1
James Moyse <james_moyse@hotmail.com>
2024-11-25 23:06:04 +0000

    More shouty coach tweaking

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,49 @@
 class QuestionsController < ApplicationController
   def home
+    @questions = [] # Placeholder
   end
 
   def ask
   end
 
   def answer
-    @answers = ["There's a game soon, get ready!", "Did you forget your kit again?", "It's raining - no game."]
-    @answer = @answers.index {|phrase.include?('!')} || @answers.index {|x| x.include?('?')} || @answers.index {|x| x.include?('.')}
+    @answers = [
+      "There's a game soon, get ready!",
+      "Did you forget your kit again?",
+      "It's raining - no game."
+    ]
+    puts "Question received: #{params[:question]}"
 
-    @question = params[:question]
+    # Get the user's question from the form input
+    @question = params[:question] || "" # Safeguard: default to an empty string if params[:question] is nil
 
+    # Select a response based on the question's content or punctuation
+    if @question.include?('!')
+      @answer = @answers[0]
+    elsif @question.include?('?')
+      @answer = @answers[1]
+    else
+      @answer = @answers[2]
+    end
   end
-
 end
+
+# class QuestionsController < ApplicationController
+
+
+#   def home
+#     @questions
+#   end
+
+#   def ask
+#   end
+
+#   def answer
+#     @answers = ["There's a game soon, get ready!", "Did you forget your kit again?", "It's raining - no game."]
+#     @answer = @answers.index {|phrase.include?('!')} || @answers.index {|x| x.include?('?')} || @answers.index {|x| x.include?('.')}
+
+#     @question = params[:question]
+
+#   end
+
+# end
